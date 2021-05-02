3bc93f52722548c0919db35be852989276288233
Roni Vayre <roni@plantoeat.com>
2021-05-02 15:48:16 -0600

    commit chapters one and two exercises"

---
+++ chap2_variables/name.rb
@@ -0,0 +1,13 @@
+puts "What's your name?"
+name = gets.chomp
+puts "Hello, " + name + "!"
+
+10.times do 
+  puts name
+end
+
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts first_name + " " + last_name
